cf5dd59dc11c288ad5816c0e605a7efec48e150b
adell702 <90898112+adell702@users.noreply.github.com>
2023-07-01 17:31:36 +0700

    Add files via upload

---
+++ Praktikum 1_Pertemuan 4_18 Maret _23/casesensitive1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO "Hell Boy!<br>";
+echo "Hello Girl!<br>";
+ECHO "Hello Gay!<br>";
+?>
+
+</body>
+</html>
---
+++ Praktikum 1_Pertemuan 4_18 Maret _23/coba.php
@@ -0,0 +1,5 @@
+<?php
+echo "Saya sedang belajar PHP di kelas Pemrograman Web Universitas Paramadina";
+echo "<br>";
+echo "Hello World...";
+?>
